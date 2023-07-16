8f51dd2ce420bb06229d774877f5b4f563603ced
Daniel Tasada <daniel.tasada@icloud.com>
2023-07-16 21:22:28 +0200

    Update

--- src/engine.py
+++ src/engine.py
@@ -1,28 +1,72 @@
 import pygame
 from pygame._sdl2.video import Window, Renderer, Texture, Image
+import sys
+from settings import *
 
-def load_texture(path, renderer, frames=1):
+
+def set_state(game, target_state):
+  game.state = target_state
+
+
+def get_mouse_pos():
+  return [pos/SCALE_CONST for pos in pygame.mouse.get_pos()]
+
+
+def load_tex(path, renderer, frames=1, scale_factor=1):
   if frames > 1:
-    surface = pygame.image.load(path)
-    frame_width = surface.get_width() / frames
-    frame_height = surface.get_height()
-    textures = [Texture.from_surface(renderer, surface.subsurface(x * frame_width, 0, frame_width, frame_height)) for x in range(frames)]
-    rectangles = [tex.get_rect(midbottom = (50, 50)) for tex in textures]
-    return textures, rectangles
+    surf = pygame.image.load(path)
+    frame_width = surf.get_width() / frames
+    frame_height = surf.get_height()
+    texs = [Texture.from_surface(renderer, surf.subsurface(x * frame_width, 0, frame_width, frame_height)) for x in range(frames)]
+    rects = [tex.get_rect(midbottom = (50, 50)) for tex in texs]
+    return texs, rects
   else:
-    surface = pygame.image.load(path)
-    texture = Texture.from_surface(renderer, surface)
-    rectangle = texture.get_rect(midbottom = (50, 50))
-    return texture, rectangle
+    surf = pygame.transform.scale_by(pygame.image.load(path), scale_factor)
+    tex = Texture.from_surface(renderer, surf)
+    rect = tex.get_rect()
+    return tex, rect
 
-class Entity():
-  def animate(self, textures, rectangles, frequency):
+
+def fill_rect(renderer, rect, color):
+  renderer.draw_color = color
+  renderer.fill_rect(rect)
+
+
+def quit(running):
+  running = False
+  pygame.quit()
+  sys.exit()
+
+
+class LinkButton:
+  def __init__(self, text, pos, size, font, color, renderer, command):
+    self.surf = font.render(text, True, color)
+    self.tex = Texture.from_surface(renderer, self.surf)
+    self.rect = self.tex.get_rect()
+    self.rect.x, self.rect.y = pos
+    self.command = command
+
+
+  def update(self, win):
+    win.renderer.blit(self.tex, self.rect)
+
+  def process_event(self, event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if self.rect.collidepoint(*get_mouse_pos()):
+        self.command()
+
+
+class Entity:
+  def animate(self, texs, rects, frequency):
     self.anim += frequency
-    if self.anim >= len(rectangles):
+    if self.anim >= len(rects):
       self.anim = 0
-    self.texture = textures[int(self.anim)]
-    self.rectangle = rectangles[int(self.anim)]
+    self.tex = texs[int(self.anim)]
+    self.rect = rects[int(self.anim)]
 
-  def base_update(self, win):
-    self.animate(self.textures, self.rectangles, 0.1)
-    win.renderer.blit(self.texture, self.rectangle)
+  def base_update(self, win, x_pos, y_pos, anim=True):
+    if anim:
+      self.animate(self.texs, self.rects, 0.1)
+    self.rect.x = x_pos
+    self.rect.y = y_pos
+    win.renderer.blit(self.tex, self.rect)
--- src/main.py
+++ src/main.py
@@ -4,9 +4,16 @@ import sys
 from engine import *
 from settings import *
 
+
 pygame.init()
 clock = pygame.time.Clock()
-font = pygame
+font_billy_regular = pygame.font.Font("../assets/billy/Billy-Regular.ttf", 20)
+
+
+class Game():
+  def __init__(self):
+    self.state = "menu"
+    self.running = True
 
 class WindowHandler:
   def __init__(self, size, vsync=0):
@@ -18,38 +25,62 @@ class WindowHandler:
     self.center = [s // 2 for s in size]
     self.renderer.scale = (SCALE_CONST, SCALE_CONST)
 
+
 class Corbin(Entity):
   def __init__(self):
     self.x_pos = win.center[0]
     self.y_pos = win.center[1]
     self.anim = 0
-    self.textures, self.rectangles = load_texture('../assets/player_run.png', win.renderer, 8)
+    self.texs, self.rects = load_tex('../assets/player-run.png', win.renderer, 8)
 
   def movement(self):
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_w:
-          self.y_pos -= 2
-        if event.key == pygame.K_a:
-          self.x_pos -= 2
-        if event.key == pygame.K_s:
-          self.y_pos += 2
-        if event.key == pygame.K_d:
-          self.y_pos += 2
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+      self.y_pos -= 2
+    if keys[pygame.K_a]:
+      self.x_pos -= 2
+    if keys[pygame.K_s]:
+      self.y_pos += 2
+    if keys[pygame.K_d]:
+      self.x_pos += 2
 
   def update(self):
-    self.base_update(win)
+    self.base_update(win, self.x_pos, self.y_pos)
     self.movement()
 
+class Menu(Entity):
+  def __init__(self):
+    self.x_pos = -440
+    self.y_pos = 0
+    self.tex, self.rect = load_tex('../assets/menu-wall.png', win.renderer, 1, 0.75)
 
+  def update(self):
+    self.x_pos += 0.1
+    self.base_update(win, self.x_pos, self.y_pos, False)
+
+
+game = Game()
 win = WindowHandler((WIN_WIDTH, WIN_HEIGHT), 1)
+menu = Menu()
 corbin = Corbin()
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
+menu_buttons = [LinkButton("Play", (win.width/15, win.height/2), (24, 24), font_billy_regular, (255, 255, 255, 255), win.renderer, lambda: set_state(game, "gameplay"))]
 
-  corbin.update()
+while game.running:
+  for event in pygame.event.get():
+    for menu_button in menu_buttons:
+      menu_button.process_event(event)
+    if event.type == pygame.QUIT:
+      quit(game.running)
+
+  fill_rect(win.renderer, (0, 0, *win.size), (0, 0, 0, 255))
+
+  if game.state == "menu":
+    menu.update()
+    for menu_button in menu_buttons:
+      menu_button.update(win)
+
+  if game.state == "gameplay":
+    corbin.update()
+
   win.renderer.present()
