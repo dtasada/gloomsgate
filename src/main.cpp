d8f2c9c9d5c63ec9034efb4fab44dc9f291e9a07
Daniel Tasada <daniel.tasada@icloud.com>
2023-10-19 21:10:10 +0200

    no window?

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,31 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
-int main(int argv, char* args[]) {
-	printf("Hello world!\n");
+#include "../include/RenderWindow.hpp"
+
+int main(int argc, char* argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning) {
+		// Get our controls and events
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
 	return 0;
 }
